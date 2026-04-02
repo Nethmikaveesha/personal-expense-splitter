be7f04eda36120ac91fdd89d70410895253de175
Kaveesha <nethmikaveesha593@gmail.com>
2026-04-02 08:35:47 +0530

    Initial commit: frontend and backend setup and add codes

---
+++ expense-splitter-backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
